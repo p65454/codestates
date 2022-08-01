fff7b70363ebf94c20b80356e8d5ddf3894ed52f
codestates <codestates@codestatess-MacBook-Pro.local>
2022-08-01 17:50:04 +0900

    init student repo

---
+++ src/Part_1.py
@@ -0,0 +1,11 @@
+"""
+Part 1
+
+아래에 코드에 요구사항에 알맞은 SQL 쿼리문을 작성합니다.
+"""
+
+CUSTOMER_TABLE = "Customer 테이블을 생성하는 SQL 쿼리문을 작성합니다."
+
+PACKAGE_TABLE = "Package 테이블을 생성하는 SQL 쿼리문을 작성합니다."
+
+CUSTOMER_PACKAGE_TABLE = "Customer_Package 테이블을 생성하는 SQL 쿼리문을 작성합니다."
