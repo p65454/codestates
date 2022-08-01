777a09aae2f9b069fe4e30eb046e2b8e15d6c632
codestates <codestates@codestatess-MacBook-Pro.local>
2022-08-01 17:52:29 +0900

    init student repo

---
+++ src/Part_2.py
@@ -0,0 +1,35 @@
+"""
+Part 2
+
+각 질문에서 명시한 요구사항을 충족하는 SQL 문을 작성합니다.
+"""
+
+QUERY_1 = "주어진 스키마에 따라 Teacher table을 생성하는 쿼리를 작성합니다."
+  
+QUERY_2 = "주어진 스키마에 따라 Student table을 생성하는 쿼리를 작성합니다.  "
+ 
+BLANK = "QUERT_3_1~7의 BLANK 부분에 들어갔을 때 Teacher 테이블에 데이터를 넣을 수 있는 문장을 작성합니다."
+
+#밑의 코드는 변경하지 않습니다.
+QUERY_3_1 = f"{BLANK} VALUES ('Spongebob', 'Math', 3500)"
+QUERY_3_2 = f"{BLANK} VALUES ('Patrick', 'Science', 3200)"
+QUERY_3_3 = f"{BLANK} VALUES ('Squidward', 'Math', 3800)"
+QUERY_3_4 = f"{BLANK} VALUES ('Sandy', 'English', 3300)"
+QUERY_3_5 = f"{BLANK} VALUES ('Nimo', 'English', 3150)"
+QUERY_3_6 = f"{BLANK} VALUES ('Dory', 'Math', 2800)"
+QUERY_3_7 = f"{BLANK} VALUES ('Marlin', 'Science', 4000)"
+
+#Student에 데이터를 넣는 쿼리입니다. 변경하지 마세요 (Part_3에서 사용)
+QUERY_4_1 = "INSERT INTO Student VALUES(1, 'C100', 50);"
+QUERY_4_2 = "INSERT INTO Student VALUES(1, 'C101', 55);"
+QUERY_4_3 = "INSERT INTO Student VALUES(1, 'C102', 56);"
+QUERY_4_4 = "INSERT INTO Student VALUES(2, 'D100', 60);"
+QUERY_4_5 = "INSERT INTO Student VALUES(2, 'D101', 72);"
+QUERY_4_6 = "INSERT INTO Student VALUES(2, 'D102', 73);"
+QUERY_4_7 = "INSERT INTO Student VALUES(2, 'D103', 73);"
+
+#Teaher 테이블에 Insert가 잘 되어있지 않다면, 해당 쿼리도 틀릴 수 있습니다
+QUERY_4 = "Urclass의 과제 설명에 제공된 이미지와 같은 결과값을 출력하는 쿼리를 작성합니다."
+
+#Teacher 테이블에 Insert 가 잘 되어있지 않다면, 해당 쿼리도 틀릴 수 있습니다
+QUERY_6 = "Teacher 테이블을 삭제하는 쿼리를 생성합니다."
