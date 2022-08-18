ac3489f7ed718767204c776bac6f8e371dd073f4
p65454 <p65454@gmail.com>
2022-08-18 19:52:15 +0900

    1stsub

--- simple_flask_app/app.py
+++ simple_flask_app/app.py
@@ -5,7 +5,8 @@ Simple Flask App 입니다.
 어떻게 동작을 하는지 살펴보세요!
 """
 import os
-from flask import Flask
+from flask import Flask, render_template
+import csv
 
 # 추가로 필요한 패키지들이 있는 경우 가져와 사용합니다.
 
@@ -24,7 +25,10 @@ def index():
     index 함수에서는 'simple_flask_app/templates' 폴더에 있는 'index.html'
     파일을 렌더링 해줘야 합니다!
     """
-    return 'OK', 200
+    
+    return render_template('index.html') , 200
+
+@app.route('/users', methods=['GET'])
 
 def users():
     """
@@ -36,10 +40,17 @@ def users():
         }
 
     NOTE: CSV 파일을 읽어온 뒤에 가장 첫 줄인 'username' 은 리스트에 담지 않습니다!  """
+    with open('users.csv') as csvfile:
+        #userlist = {[csv.reader(csvfile)]}
+        user_csv = csv.reader(csvfile)
+        userlist = {'users': [row[0] for row in user_csv][1:]}
+        
 
-    return 'OK', 200
+    return userlist, 200
 
 
+@app.route('/users/', defaults={'user_order':1})
+@app.route('/users/<user_order>')
 def display_user(user_order):
     """
     display_user 함수에서는 사용자가 '서버 주소 + /users/{ 유저 아이디 }' 로
@@ -63,7 +74,10 @@ def display_user(user_order):
     -   예를 들어 사용자가 '/users/2' 에 접속하면 '2' 번째, 즉 두 번째 유저인
         'patrick' 문자열을 돌려줍니다.
     """
-    return 'OK', 200
+    user_dic,_ = users()
+    username = user_dic['users'][int(user_order)-1]
+
+    return username, 200
     
 
 if __name__ == "__main__":
