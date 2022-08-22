861c4a94e40c5bfb5337f9b3828136449dfbe1c6
TetorCo <jaksim7531@naver.com>
2022-08-23 00:44:45 +0900

    init student repo

---
+++ sprint_challenge/Part_3.py
@@ -0,0 +1,11 @@
+"""
+docker container에 저장된 환경변수 ANSWER 을 찾아서 입력하세요
+- 이미지 생성자 : aibcontents
+- 이미지 이름 : sc_docker
+- 태그 : 1.0
+- 환경변수 출력 명령(COMMAND) : env
+"""
+
+def env_of_sc33():
+    answer = ""
+    return answer
